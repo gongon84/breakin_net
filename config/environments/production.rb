bb0245ae8960a61ed81e80da9fe3d34da5da66c5
gongon84 <shinya7102@gmail.com>
2021-04-13 17:28:12 +0900

    メール機能　本番環境用　追記

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,17 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   # メール送信機能　追記
+  config.action_mailer.default_url_options = { host: 'https://breakin-net.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :user_name => ENV['GMAIL_USERNAME'],                   
+    :password => ENV['GMAIL_PASSWORD'],                    
+    :domain => 'https://breakin-net.herokuapp.com',
+    :address => 'smtp.gmail.com',                          
+    :port => 587,
+    :authentication => :plain,                                
+    :enable_starttls_auto => true    
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
