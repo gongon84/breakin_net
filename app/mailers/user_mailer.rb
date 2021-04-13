d372b83c8c5a07e707cff65e4c18dfcc72c23c2a
gongon84 <shinya7102@gmail.com>
2021-04-13 18:30:07 +0900

    メール機能　本番環境用　追記

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,7 @@
 class UserMailer < ApplicationMailer
 
+  default from: ENV['GMAIL_USERNAME']
+
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
   #
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,7 +69,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     :user_name => ENV['GMAIL_USERNAME'],                   
     :password => ENV['GMAIL_PASSWORD'],                    
-    :domain => 'https://breakin-net.herokuapp.com',
+    :domain => 'gmail.com',
     :address => 'smtp.gmail.com',                          
     :port => 587,
     :authentication => :plain,                                
