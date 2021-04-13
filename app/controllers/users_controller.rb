0199bea72a78dde268810c06cec84e42f2df985d
gongon84 <shinya7102@gmail.com>
2021-04-14 00:37:15 +0900

    メール機能 2通届くエラー　修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,11 +27,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      logger.debug('save成功しました')
       # UserMailer.activation_needed_email(@user).deliver_now
       redirect_to("/users/index", notice: 'メールを送信しました。そちらからログインください')
     else
-      logger.debug('save失敗しました')
       flash.now[:alert] = '登録に失敗しました'
       render 'users/new'
     end
@@ -55,7 +53,7 @@ class UsersController < ApplicationController
     # アクセストークンの認証
     if (@user = User.load_from_activation_token(params[:id]))
       @user.activate!
-      UserMailer.activation_success_email(@user).deliver_now
+      # UserMailer.activation_success_email(@user).deliver_now
       redirect_to(login_path, :notice => '登録が完了しました')
     else
       not_authenticated
